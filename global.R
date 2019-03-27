482aeb24cafe02916bf96b050c7211b8b592078c
Phileas Condemine <phileas.condemine@gmail.com>
2019-03-27 17:38:35 +0100

    ajout des metadata dims et amelioration des inputs UI/UX

--- global.R
+++ global.R
@@ -3,5 +3,53 @@ library(DT)
 library(shinydashboard)
 library(shinycssloaders)
 library(dplyr)
-load("files_infos.RData")
+library(leaflet)
+library(shinyWidgets)
+# library(shinyBS)
+library(bsplus)
+library(reactlog)
+# tell shiny to log reactivity
+options("shiny.reactlog" = TRUE)
+# library(shinyloadtest)
+load("data/files_infos.RData")
+# on mélange l'ordre des tables pour ne pas avantager certaines
+files_infos$random_order=sample(nrow(files_infos))
+setorder(files_infos,random_order)
+
 load("../metadata_IVT.RData")
+load("../dimension_lib2.RData")
+# utils/prep_prolygons.R
+load("data/FR_gadm.RData")
+# source("utils/find_readable_files.R")
+FRA_dep_1pct@data$ordre=1:nrow(FRA_dep_1pct@data)
+
+
+tag.map.title <- tags$style(HTML("
+  .leaflet-control.map-title { 
+                                 transform: translate(-50%,20%);
+                                 position: fixed !important;
+                                 left: 50%;
+                                 text-align: center;
+                                 padding-left: 10px; 
+                                 padding-right: 10px; 
+                                 background: rgba(255,255,255,0.75);
+                                 font-weight: bold;
+                                 font-size: 28px;
+                                 }
+                                 "))
+
+fix_noms_duplicated=function(headers){
+  dup_dt=data.table(noms=headers,dup=duplicated(headers))
+  dup_dt[,dup_nb:=cumsum(dup),by="noms"]
+  dup_dt$dup_nb=as.character(dup_dt$dup_nb)
+  dup_dt[dup_nb=="0",dup_nb:=""]
+  paste0(dup_dt$noms,dup_dt$dup_nb)
+}
+
+# do.call mean vs sum ne se comporte pas de la même manière
+# https://www.reddit.com/r/rstats/comments/6igdrr/for_some_reason_docallmean_x_does_not_work/
+mean2=function (..., na.rm = FALSE){
+  mean(x=c(...),na.rm=na.rm)
+}
+  
+  
--- server.R
+++ server.R
@@ -79,20 +79,84 @@ function(input,output,session){
       # print(noms_donnees)
       # print("donnees de croisement")
       # print(vars$txt[!vars$txt=="DONNEES"])
-      names(fulldata)<-c(vars$txt[!vars$txt=="DONNEES"],noms_donnees)
+      names(fulldata)<-c(vars$txt[!grepl("DONNEES",vars$txt)],noms_donnees)
       
       
       }
     fulldata=fulldata%>%data.table
   })
+  
+  dimension_metadata=reactive({
+    if(length(input$list_fichiers_cell_clicked)>0){
+      print("metadata_dim")
+      clicked=input$list_fichiers_cell_clicked
+      row_clicked=clicked$row
+      content=files_infos[row_clicked]
+      my_href=content$href
+      print(my_href)
+      print(sapply(dimension_lib2,class))
+      
+      dimension_metadata=dimension_lib2[href==my_href]
+      dimension_metadata[!is.na(Description)&!is.na(Notes),Description:=
+                           paste(Description,Notes,sep="\n")]
+      dimension_metadata[is.na(Description)&!is.na(Notes),Description:=Notes]
+      dimension_metadata[is.na(Description)&is.na(Notes),Description:=Dimension]    
+      dimension_metadata[,c("Dimension","Description"),with=F]
+      
+      } else {NULL}
+  })
+  
+  sketch=reactive({
+    nms=data.table(Dimension=names(mydata()),
+                   ordre=1:length(names(mydata())),
+                   stringsAsFactors = F)
 
+    dimension_metadata=dimension_metadata()
+    # print(dimension_metadata()$Dimension)
+
+    
+    # print(nms)
+    print(dimension_metadata[,c("Dimension","Description"),with=F])
+    dimension_metadata=merge(nms,dimension_metadata,by="Dimension",all.x=T)
+    print(dimension_metadata[,c("Dimension","Description"),with=F])
+    dimension_metadata[is.na(Description),Description:=Dimension]
+    
+    setorder(dimension_metadata,ordre)
+    tooltips=sprintf('<th title="%s">%s</th>',
+                     dimension_metadata$Description,
+                     dimension_metadata$Dimension)
+    tooltips=paste(tooltips,collapse=" ")
+    tooltips=paste('<table class="display"><thead><tr>',
+                   tooltips,
+                   '</tr></thead></table>')
+    tooltips
+
+    
+    
+  })
   
   mydata=reactive({
     req(fulldata())
     mydata=fulldata()
-    mydata2<<-mydata
+    # mydata2<<-mydata
     #On filtre parmi les stats pré-calculées pour obtenir le niveau hiérarchique souhaité
     if (!(is.null(input$operation)|is.null(input$join_vars))){
+      if ("supp zéros"%in%input$rm_zero){
+        vars_to_check=setdiff(names(mydata),variables_a_croiser())
+        DONNEES_cols=mydata[,vars_to_check,with=F]
+        print(paste("nombre de colonnes de stats"),ncol(DONNEES_cols))
+        mydata=mydata[!rowSums(DONNEES_cols)==0]
+      }
+      if ("supp NA"%in%input$rm_zero){
+        vars_to_check=setdiff(names(mydata),variables_a_croiser())
+        DONNEES_cols=mydata[,vars_to_check,with=F]
+        print(paste("nombre de colonnes de stats"),ncol(DONNEES_cols))
+        mydata=mydata[!is.na(rowSums(DONNEES_cols))]
+      }
+
+      
+      
+      
       metadata=metadata()
       vars=rbind(metadata$vars_in_cols,metadata$vars_in_rows,metadata$vars_in_others)
       vars_hc<<-vars
@@ -143,7 +207,10 @@ function(input,output,session){
     
     
       if(length(setdiff(variables_a_croiser(),input$join_vars))>0){
-        mydata=mydata[,setdiff(names(mydata),setdiff(variables_a_croiser(),input$join_vars)),with=F][
+        vars_stats_et_joins=setdiff(names(mydata),setdiff(variables_a_croiser(),input$join_vars))
+        print(vars_stats_et_joins)
+        print(setdiff(vars_stats_et_joins,input$join_vars))
+        mydata=mydata[,vars_stats_et_joins,with=F][
         ,do.call(input$operation,.SD),by=eval(input$join_vars)]
         setnames(mydata,paste0("V",1:length(changeCols)),changeCols)
       print("grouping done !")
@@ -185,6 +252,8 @@ function(input,output,session){
     sub_items=list()
     #### CONSTRUCTION DU CHOIX HIERARCHIQUE SUR LES VARIABLES
     input_hierarchies=tagList()
+    # input_hierarchies[[1]] = tagList()
+    # input_hierarchies[[1]][[1]] = tags$label("Granularité",class="control-label")
     metadata=metadata()
     vars=rbind(metadata$vars_in_cols,metadata$vars_in_rows,metadata$vars_in_others)
     vars$value=as.character(vars$value)
@@ -195,10 +264,30 @@ function(input,output,session){
       var_nm=gsub(" ","_",var_nm)
       hierarchie=metadata$hierarchies[[var_ind]]
       if(ncol(hierarchie)>1){
-        input_hierarchies[[i]] = tagList()
-        input_hierarchies[[i]][[1]] = selectizeInput(paste0("cols",var_nm), 
-                                       paste("Hierarchie",vars$txt[ind]), 
-                                       choices = 1:ncol(hierarchie))
+        # input_hierarchies[[i]] =  sliderInput(inputId = paste0("cols",var_nm),
+        #                                             label=NULL,
+        #                                              min = 1,max=ncol(hierarchie),step = 1,value=1,
+        #                                           ticks = F) %>%
+        #   shinyInput_label_embed(
+        #     icon("info") %>%
+        #       bs_embed_tooltip(title = paste(vars$txt[ind]))
+        #   )
+        
+
+
+        
+
+        input_hierarchies[[i]] = tags$div(style="margin:0;padding-top:0;",
+          bs_button(var_nm, button_type = "info") %>%
+            bs_attach_collapse(id_collapse = paste0("hierarchie",var_nm)),
+          bs_collapse(
+            id = paste0("hierarchie",var_nm), 
+            content = sliderInput(inputId = paste0("cols",var_nm),
+                                  label=NULL,
+                                  min = 1,max=ncol(hierarchie),step = 1,value=1,
+                                  ticks = F)
+          ))
+
         i=i+1
       }
     }
@@ -206,31 +295,67 @@ function(input,output,session){
     sub_items[[1]]=menuSubItem(icon = NULL,tabName = "overview",
                                div(id="sheet_selection_div",
                                    selectizeInput("join_vars", "Groupement par variables",
-                                           choices = variables_a_croiser(),selected = variables_a_croiser(),multiple=T,
-                                           width = '95%'),
-                                   selectizeInput("operation", "Fonction de regroupement",
-                                          choices = c("sum","mean"),selected = variables_a_croiser(),
-                                          width = '95%'),
-                                   input_hierarchies
+                                           choices = variables_a_croiser(),selected = variables_a_croiser(),multiple=T)%>%
+                                     shinyInput_label_embed(
+                                       icon("question-circle") %>%
+                                         bs_embed_tooltip(title = "Quel croisement de variables vous intéresse ?")
+                                     ),
+                                   selectizeInput(inputId = "rm_zero",label = "Pré-traitements",
+                                                  choices=c("supp zéros","supp NA"),multiple=T,
+                                                  selected=c("supp zéros","supp NA"))%>%
+                                     shinyInput_label_embed(
+                                       icon("question-circle") %>%
+                                         bs_embed_tooltip(title = "Souhaitez vous supprimer les lignes contenant des valeurs non numériques ? Elles pourraient poser problème lors du calcul de moyennes.")
+                                     ),
+                                   
+                                   
+                                   selectizeInput("operation", "Fonction de groupement",
+                                          choices = c("somme"="sum","moyenne"="mean2"),selected = variables_a_croiser())        %>%
+                                     shinyInput_label_embed(
+                                       icon("question-circle") %>%
+                                         bs_embed_tooltip(title = "Afin d'agréger les observations il sera parfois nécessaire de recourir à des opérations statistiques somme, moyenne ou autre.")
+                                     ),
+                                   div(id="hierarchies",
+                                       class="form-group shiny-input-container",
+                                       tags$label(class="control-label","Granularité/détail"),
+                                       input_hierarchies)%>%
+                                    shinyInput_label_embed(
+                                    icon("question-circle") %>%
+                                      bs_embed_tooltip(title = "Par défaut toutes les variables sont agrégées.\n
+                                                               Cliquer sur la variable dont vous souhaitez ajuster la granularité.\n
+                                                               Un menu coulissant vous permettra de régler du plus agrégé au plus granulaire."))
                                    
                                    ))
-    sub_items[[2]]=menuSubItem(icon = NULL,
+    sub_items[[2]]=menuSubItem(icon = NULL,tabName = "Visualisation",
                                div(id="choix_graph_div",
                                    selectInput("choix_graph", "Quel graphique utiliser ?",selected = F,multiple=F,
                                             selectize=F,size=3,# https://stackoverflow.com/questions/24175997/force-no-default-selection-in-selectinput/26132906#26132906
-                                            choices = c("Carte","Série temporelle","Barres"),
-                                            width = '95%')))
+                                            choices = c("Carte","Série temporelle","Barres"))%>%
+                                     shinyInput_label_embed(
+                                       icon("question-circle") %>%
+                                         bs_embed_tooltip(title = "Afin de valoriser ce jeu de données nous vous proposons plusieurs vues standards"))))
     my_list[[2]] = menuItem(text = "Aperçu des données", 
                             icon = icon("table"),startExpanded = T,sub_items)    
-    my_list[[3]]=menuItem(text = "Affichage de la visualisation", 
-                               tabName = "Visualisation",icon = icon("chart-area"))
+    # my_list[[3]]=menuItem(text = "Affichage de la visualisation", 
+    #                            icon = icon("chart-area"))
     
 
     output$menu <- renderMenu(sidebarMenu(my_list,id = "tabs"))
     updateTabItems(session = session,inputId = "tabs",selected = "overview")
   })
+  
+  output$overview_title=renderUI({
+    req(input$list_fichiers_cell_clicked)
+    if(length(input$list_fichiers_cell_clicked)>0){
+      clicked=input$list_fichiers_cell_clicked
+      row_clicked=clicked$row
+      content=files_infos[row_clicked]
+    tags$h3(content$title)
+    }
+  })
+  
   output$overview=renderDT({
-    
+    sketch=sketch()
     mydata=mydata()
     vars_char=sapply(mydata,is.character)
     vars_char=names(vars_char)[vars_char]
@@ -248,13 +373,12 @@ function(input,output,session){
                  scrollX=F,
                  pageLength = 50
                ),
-               class = "display",selection = 'single',rownames=F
+               class = "display",selection = 'single',rownames=F,container=sketch
 )})
   
   
   observeEvent(input$choix_graph=="Carte",{
     req(input$choix_graph=="Carte")
-    # CHECK SI UNE SEULE VARIABLE DE CROISEMENT EST SELECTIONNEE, ELLE DEVRAIT ETRE UNE VAR GEO
     print(variables_a_croiser())
     print("do map")
     if(length(input$join_vars)==1){
@@ -297,7 +421,7 @@ function(input,output,session){
       })
       removeUI(selector = "#map",immediate = T)
       insertUI(selector = "#dataviz_div",where = "afterBegin",ui = 
-                 tags$div(id="map",leafletOutput("map",height=800)))
+                 tags$div(id="map",withSpinner(leafletOutput("map",height=800),size = 2)))
       updateTabItems(session = session,inputId = "tabs",selected = "Visualisation")
       },error=function(e){
         print(e)
